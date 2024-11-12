a6957b427015b9b6df34ab774af07fca448da455
Angela <wpfkf4644@gmail.com>
2024-11-12 15:19:26 +0900

    Feat: 백엔드 세팅 완료_스프링부트 3.3.5

--- KDTMooluck/src/main/java/org/kdt/mooluck/interaction/service/InteractionService.java
+++
@@ -1,9 +0,0 @@
-package org.kdt.mooluck.interaction.service;
-
-import org.kdt.mooluck.interaction.dto.InteractionDTO;
-
-import java.util.List;
-
-public interface InteractionService {
-    List<InteractionDTO> getInteractionList(Integer elderId);
-}
