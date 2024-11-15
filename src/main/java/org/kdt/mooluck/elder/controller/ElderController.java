c137643b8ad936e860c88f74825aafb852cc3045
DYA <sally7410@naver.com>
2024-11-15 15:01:51 +0900

    Fix: ElderController Response 형식 변경

--- src/main/java/org/kdt/mooluck/elder/controller/ElderController.java
+++ src/main/java/org/kdt/mooluck/elder/controller/ElderController.java
@@ -1,5 +1,6 @@
 package org.kdt.mooluck.elder.controller;
 
+import org.kdt.mooluck.custom.CustomResponse;
 import org.kdt.mooluck.elder.dto.ElderDTO;
 import org.kdt.mooluck.elder.service.ElderService;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +22,23 @@ public class ElderController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody ElderDTO elderDto) {
+    public ResponseEntity<CustomResponse> login(@RequestBody ElderDTO elderDto) {
         boolean isValid = elderService.validateMember(elderDto);
         if (isValid) {
-            return ResponseEntity.ok("Login successful");
+            return ResponseEntity.ok(CustomResponse.success("Login successful"));
         } else {
-            return ResponseEntity.status(401).body("Invalid credentials");
+            return ResponseEntity.status(401).body(CustomResponse.message("Invalid credentials"));
         }
     }
+
+
+//    @PostMapping("/login")
+//    public ResponseEntity<String> login(@RequestBody ElderDTO elderDto) {
+//        boolean isValid = elderService.validateMember(elderDto);
+//        if (isValid) {
+//            return ResponseEntity.ok("Login successful");
+//        } else {
+//            return ResponseEntity.status(401).body("Invalid credentials");
+//        }
+//    }
 }
