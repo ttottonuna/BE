1eaf95a7a0d661f681862e2d0a3e28cbaa9b7ab9
kogunwoo <wnwn8945@naver.com>
2024-11-18 21:30:57 +0900

    ãfix : 병합 충돌 해결

--- src/main/java/org/kdt/mooluck/config/WebConfig.java
+++ src/main/java/org/kdt/mooluck/config/WebConfig.java
@@ -1,9 +1,5 @@
 package org.kdt.mooluck.config;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 26423bc (feat : admin login/signup/logout)
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,17 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-<<<<<<< HEAD
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+
+                .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
 }
-=======
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
 
-    }
-}
->>>>>>> 26423bc (feat : admin login/signup/logout)
