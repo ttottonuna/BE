9ebefb2df6987afc5d279443544bb780efffed2e
Cho Yeonsoo <meriel1010@hanyang.ac.kr>
2024-11-20 17:10:06 +0900

    24-11-20 연수 Feat: 시간별선그래프, elderID별 막대그래프, 스케줄러로 데일리 리셋

--- src/main/java/org/kdt/mooluck/config/WebConfig.java
+++ src/main/java/org/kdt/mooluck/config/WebConfig.java
@@ -1,10 +1,12 @@
 package org.kdt.mooluck.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableScheduling
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -16,4 +18,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600);
     }
 }
+
 
